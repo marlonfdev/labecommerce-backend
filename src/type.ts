96032534b11e91ea306d723f68309df012a5ed73
Marlon Ferreira <marlonf.dev@gmail.com>
2023-07-14 12:25:11 -0300

    feat: Implementação das tipagens e dados mock

---
+++ src/type.ts
@@ -0,0 +1,16 @@
+export type User = {
+    id: string;
+    name: string;
+    email: string;
+    password: string;
+    createdAt: string;
+  };
+  
+  export type Product = {
+    id: string;
+    name: string;
+    price: number;
+    description: string;
+    imageUrl: string;
+  };
+  
